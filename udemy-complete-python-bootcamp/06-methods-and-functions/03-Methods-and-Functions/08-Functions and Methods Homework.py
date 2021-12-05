ffd3a7d3ae638c5b109edd1f3b454b138e644ee7
Chris Chua <weitat@gmail.com>
2021-12-05 15:57:29 +0000

    20211205_1157

--- udemy-complete-python-bootcamp/06-methods-and-functions/03-Methods-and-Functions/08-Functions and Methods Homework.py
+++ udemy-complete-python-bootcamp/06-methods-and-functions/03-Methods-and-Functions/08-Functions and Methods Homework.py
@@ -8,6 +8,8 @@ def vol(rad):
     return (4 / 3) * 3.14 * (rad ** 3) 
 
 print(vol(2))
+
+
 # %%
 '''Write a function that checks whether a 
 number is in a given range (inclusive of 
@@ -25,6 +27,8 @@ def ran_bool(num, low, high):
     return low <= num <= high
 
 print(ran_bool(3,1,10))
+
+
 # %%
 '''Write a Python function that accepts a 
 string and calculates the number of upper 
@@ -42,4 +46,64 @@ def up_low(s):
 
 s = 'Hello Mr. Rogers, how are you this fine Tuesday?'
 up_low(s)
+
+
 # %%
+'''Write a Python function that takes a 
+list and returns a new list with unique 
+elements of the first list.
+'''
+def unique_list(lst):
+    return list(set(lst))
+
+print(unique_list([1,1,1,1,2,2,3,3,3,3,4,5]))
+
+
+# %%
+'''Write a Python function to multiply 
+all the numbers in a list.
+'''
+def multiply(numbers):
+    total = 1
+    for x in numbers:
+        total *= x
+    return total
+
+print(multiply([1,2,3,-4]))
+
+
+# %%
+'''Write a Python function that checks 
+whether a word or phrase is palindrome 
+or not.
+'''
+def palindrome(s):
+    s = s.replace(' ', '')
+    mid_point = (len(s) + 1) //2
+    first_half = s[:mid_point]
+    second_half = s[mid_point:]
+    rev_second_half = second_half[::-1]
+    return first_half == rev_second_half
+
+print(palindrome('helleh'))
+
+
+# %%
+## Hard
+
+'''Write a Python function to check 
+whether a string is pangram or not. 
+(Assume the string passed in does not 
+have any punctuation). 
+
+Pangrams are words or sentences containing 
+every letter of the alphabet at least once.
+'''
+import string
+
+def ispangram(str1, alphabet=string.ascii_lowercase):
+    str1 = str1.replace(' ', '').lower()
+    unique_str1 = set(str1)
+    return unique_str1 == set(alphabet)
+
+print(ispangram("The quick brown fox jumps over the lazy dog"))
